ac34a12f63e38a874e39ca65b7c55a4975815f20
Carl-ZLJ <zljmms32@gmail.com>
2024-01-12 11:36:05 +0800

    add git push and pull keymaps

--- lua/carl/lazy/fugitive.lua
+++ lua/carl/lazy/fugitive.lua
@@ -1,7 +1,9 @@
 return {
-    'tpope/vim-fugitive',
+    "tpope/vim-fugitive",
     config = function()
-        vim.keymap.set("n", "<leader>hg", ":G<CR>", { desc = 'Fugitive Git'})
-        vim.keymap.set("n", "<leader>hl", ":G log<CR>", { desc = 'Fugitive Git Log'})
+        vim.keymap.set("n", "<leader>hg", ":G<CR>", { desc = "Fugitive Git" })
+        vim.keymap.set("n", "<leader>hl", ":G log<CR>", { desc = "Fugitive Git Log" })
+        vim.keymap.set("n", "<leader>hr", ":G pull --rebase", { desc = "Fugitive Git Pull Rebase" })
+        vim.keymap.set("n", "<leader>hP", ":G push -u origin", { desc = "Fugitive Git Push" })
     end,
 }
