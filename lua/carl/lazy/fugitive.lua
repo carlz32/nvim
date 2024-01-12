225e038ac153421cc5165c4d1353e3472ce6cdba
Carl-ZLJ <zljmms32@gmail.com>
2024-01-12 11:19:48 +0800

    add fugitive keymap

--- lua/carl/lazy/fugitive.lua
+++ lua/carl/lazy/fugitive.lua
@@ -1,6 +1,7 @@
 return {
     'tpope/vim-fugitive',
     config = function()
-        vim.keymap.set("n", "<leader>hg", vim.cmd.Git, { desc = 'Fugitive Git'})
+        vim.keymap.set("n", "<leader>hg", ":G<CR>", { desc = 'Fugitive Git'})
+        vim.keymap.set("n", "<leader>hl", ":G log<CR>", { desc = 'Fugitive Git Log'})
     end,
 }
