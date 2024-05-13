a1d1fd0e188fde4775f6d67a5dd5d9c5d426bcd8
Carl-ZLJ <zljmms32@gmail.com>
2024-05-13 10:52:17 +0800

    make neovim happy

--- lua/carl/lazy/none-ls.lua
+++ lua/carl/lazy/none-ls.lua
@@ -5,11 +5,11 @@ return {
         null_ls.setup({
             sources = {
 
-                null_ls.builtins.formatting.prettierd.with({
-                    filetypes = { "html", "json", "yaml", "markdown" },
-                }),
+                -- null_ls.builtins.formatting.prettierd.with({
+                --     filetypes = { "html", "json", "yaml", "markdown" },
+                -- }),
 
-                null_ls.builtins.code_actions.refactoring,
+                -- null_ls.builtins.code_actions.refactoring,
             },
         })
     end,
