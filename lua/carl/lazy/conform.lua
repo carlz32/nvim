2a332572b39582332d7e7370fcb3388e7d5103d6
carlz32 <zljmms32@gmail.com>
2024-10-17 11:02:36 +0800

    remove prettierd because of cli error(no files specified in neovim)

--- lua/carl/lazy/conform.lua
+++ lua/carl/lazy/conform.lua
@@ -6,9 +6,9 @@ return {
             formatters_by_ft = {
                 lua = { "stylua" },
                 python = { "isort", "black" },
-                javascript = { "prettierd", "prettier", stop_after_first = true },
-                html = { "prettierd", "prettier", stop_after_first = true },
-                markdown = { "prettierd", "prettier", stop_after_first = true },
+                javascript = { "prettier", stop_after_first = true },
+                html = { "prettier", stop_after_first = true },
+                markdown = { "prettier", stop_after_first = true },
             },
             format_on_save = {
                 timeout_ms = 500,
