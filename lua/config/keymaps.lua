562efa73c4ec40e74160b6a3fa1d6f66d5c853f4
carlz32 <zljmms32@gmail.com>
2025-02-10 13:27:03 +0800

    super-tab key setup

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,11 +8,17 @@ vim.g.mapleader = " "
 vim.keymap.set("i", "jk", "<Esc>", { desc = "Exit Insert Mode" })
 vim.keymap.set("n", "<Esc>", vim.cmd.nohl, { desc = "No Highlight" })
 
--- Buffer
-vim.keymap.set("n", "<tab>", "<CMD>bnext<CR>", { desc = "Next Buffer" })
-vim.keymap.set("n", "<S-tab>", "<CMD>bprevious<CR>", { desc = "Previous Buffer" })
-
 -- Smart copy and paste
-vim.keymap.set({ "n", "v" }, "x", '"_x', { desc = "Cut without Yank" })
-vim.keymap.set({ "n", "v" }, "<leader>d", '"_d', { desc = "Delete without Yank" })
-vim.keymap.set({ "n", "v" }, "<leader>p", '"_dP', { desc = "Paste without Yank" })
+vim.keymap.set({ "n", "v" }, "x", "\"_x", { desc = "Cut without Yank" })
+vim.keymap.set({ "n", "v" }, "<leader>d", "\"_d", { desc = "Delete without Yank" })
+vim.keymap.set({ "n", "v" }, "<leader>p", "\"_dP", { desc = "Paste without Yank" })
+
+vim.keymap.set("n", "J", "mzJ`z", { desc = "Smart Join" })
+
+-- Move Lines
+vim.keymap.set("n", "<A-j>", "<cmd>execute 'move .+' . v:count1<cr>==", { desc = "Move Down" })
+vim.keymap.set("n", "<A-k>", "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = "Move Up" })
+vim.keymap.set("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
+vim.keymap.set("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
+vim.keymap.set("v", "<A-j>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = "Move Down" })
+vim.keymap.set("v", "<A-k>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Up" })
