0892c04dcf27c5105cd810b52a026955a5e85a41
carlz32 <zljmms32@gmail.com>
2025-02-12 18:45:51 +0800

    chatanywhere

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -11,45 +11,30 @@ return {
             temperature = 0,
             max_tokens = 4096,
         },
-        file_selector = {
-            provider = "snacks",
-            provider_opts = {},
-            compat = {
-                "avante_commands",
-                "avante_mentions",
-                "avante_files",
-            },
-        },
     },
-    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
     build = "make",
-    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
     dependencies = {
         "stevearc/dressing.nvim",
         "nvim-lua/plenary.nvim",
         "MunifTanjim/nui.nvim",
-        "hrsh7th/nvim-cmp",
         "echasnovski/mini.icons",
-        "zbirenbaum/copilot.lua", -- for providers='copilot'
+        "Saghen/blink.cmp",
+        "zbirenbaum/copilot.lua",
         {
-            -- support for image pasting
             "HakonHarnes/img-clip.nvim",
             event = "VeryLazy",
             opts = {
-                -- recommended settings
                 default = {
                     embed_image_as_base64 = false,
                     prompt_for_file_name = false,
                     drag_and_drop = {
                         insert_mode = true,
                     },
-                    -- required for Windows users
                     use_absolute_path = true,
                 },
             },
         },
         {
-            -- Make sure to set this up properly if you have lazy=true
             "MeanderingProgrammer/render-markdown.nvim",
             opts = {
                 file_types = { "markdown", "Avante" },
